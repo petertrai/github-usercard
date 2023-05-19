4e311bc7fcd161281e29b195adfee7a2f69a4af1
petertrai <petertrai@gmail.com>
2023-05-19 08:42:04 -0700

    commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get(`https://api.github.com/users/petertrai`);
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,6 +50,7 @@ const followersArray = [];
       </div>
     </div>
 */
+
 
 /*
   List of LS Instructors Github username's:
